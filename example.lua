4f1aeac6a402e7a8d9b5b381bfbe5e1476ff93cb
David Briscoe <idbrii@gmail.com>
2022-01-19 15:03:02 -0800

    Make example more comprehensive

--- example.lua
+++ example.lua
@@ -1,44 +1,72 @@
-local self = {
-    kitchen = {},
+local Person = {
+	kitchen = {},
+	none = nil,
 }
 
-if false then
-    if true then
-        local data = self.kitchen
-            :getContents()
-            :getCheese()
-            :enjoyCheese(self
-                :mouth()
-                :open()
-                :extendTongue())
-            :eatCheese({fn = function()
-                return 10
-            end})
-        data = self.kitchen
-            :getContents()
-            :getCheese()
-            :digestCheese(function()
-                local a = (1 + 2
-                    + 3
-                    * (10
-                    + 20))
-                return a
-            end, function()
+local BIGONE = "define"
 
-                return function()
-                    return 3+3
-                end
-            end)
+local b = true
+local i = 10
+local f = 10.0
+local x = 19.9e19
+local t = {
+	x = 19.9e19,
+}
 
-        -- These should display as errors.
-        self.Func()
-        self.IsInCommentOrString(10)
-
-        -- These are not errors.
-        self.func()
-        self.is_in_comment_or_string(10)
-        self:Func()
-        self:IsInCommentOrString(10)
-    end
+function Person:doStuff()
+	if false and true or (false ~= true) then
+		if true then
+			local data = self.kitchen
+				:getContents()
+				:getCheese()
+				:enjoyCheese(self
+					:mouth()
+					:open()
+					:extendTongue())
+				:eatCheese({fn = function()
+					return 10
+				end})
+			data = self.kitchen
+				:getContents(data)
+				:getCheese()
+				:digestCheese(function()
+					local a = (1 + 2
+						+ 3
+						* (10
+						+ 20))
+					return a
+				end, function()
+	
+					return function()
+						return 3+3
+					end
+				end)
+	
+			-- These should display as errors.
+			self.Func()
+			self.IsInCommentOrString(10)
+	
+			-- These are not errors.
+			self.func()
+			self.is_in_comment_or_string(10)
+			self:Func()
+			self:IsInCommentOrString(10)
+		end
+		local d = self.kitchen
+			:getContents()
+			:getCheese(function()
+				return 10
+			end)
+		return d
+	end
+	local d = self.kitchen
+		:getContents()
+		:getCheese()
+	return d
 end
 
+-- Error highlight
+t[0] = 39
+i += 39
+b = b != false
+local e = ("hello"):gsub("\w")
