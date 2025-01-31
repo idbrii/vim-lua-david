3f9dfa7ca4eb8866f2b4537124c8c944a0bf83f7
David Briscoe <idbrii@gmail.com>
2025-01-31 09:47:55 -0800

    Add a function to reload other modules
    So I can reload dap too.

--- lua/david/reload.lua
+++ lua/david/reload.lua
@@ -1,13 +1,17 @@
 local reload = {}
 
-function reload.ReloadAll()
+function reload.UnloadMatching(name_regex)
     -- https://stackoverflow.com/a/72504767/79125
     -- Clear all my modules and reload my nvim config.
     for name,_ in pairs(package.loaded) do
-        if name:match("^david") then
+        if name:match(name_regex) then
             package.loaded[name] = nil
         end
     end
+end
+
+function reload.ReloadAll()
+    reload.UnloadMatching("^david")
 
     -- Avoid my warning about unnecessary work.
     vim.cmd.unlet({"did_load_filetypes", bang = true})
